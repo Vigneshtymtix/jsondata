ad54d6f4b927a8f2c2c3039a73be0ca550d54c90
Vigneshtymtix <78462879+Vigneshtymtix@users.noreply.github.com>
2021-11-22 22:47:53 +0530

    Update scrctrl.py
    we

--- scrctrl.py
+++ scrctrl.py
@@ -13,7 +13,6 @@ import fcntl
 import struct
 import numpy as np
 import json
-import copy
 
 ############################################################################
 def getHwAddr(ifname):
@@ -33,21 +32,17 @@ video_y_size = 1080
 flip_image = 0
 rotate_image = 0
 masks = []
-show_mask = 0
 
 farmewin = 30
 frameNo = 0
 ftimes = []
-
-cameraType = 0;
-
 for i in range(farmewin):
 	ftimes.append(time.time()-(farmewin-i))
 	
 
 ############################################################################
 def readcamerasettings():	
-	global still_time, still_interval,motion_area,motion_threshold,flip_image,masks,rotate_image,show_mask
+	global still_time, still_interval,motion_area,motion_threshold,flip_image,masks,rotate_image
 	myvars = {}
 	if os.path.isfile("camsettings.txt"):
 		with open("camsettings.txt") as myfile:
@@ -61,7 +56,6 @@ def readcamerasettings():
 			still_interval = myvars['STILL_INTERVAL']
 			flip_image = myvars['FLIP_IMAGE']
 			rotate_image = myvars['ROTATE_IMAGE']
-			show_mask = 4*myvars['SHOWMASK']
 			
 			
 	print "Motion Area: ",motion_area
@@ -70,48 +64,46 @@ def readcamerasettings():
 	print "Still Interval: ",still_interval
 	print "Flip Image: ",flip_image
 	print "Rotate Image: ",rotate_image
-	print "Show Mask: ",show_mask
 	
 	
-	if os.path.isfile("campmask.txt"):
-		with open("campmask.txt") as myfile:
-			for line in myfile: 
+	if os.path.isfile("cammask.txt"):
+		with open("cammask.txt") as myfile:
+			for line in myfile:
 				print line								#For Debugging
 				temp1 = line.split("|")
 				for m in temp1:
-					mask = np.fromstring(m,dtype=float,sep=',')
+					mask = np.fromstring(m,dtype=int,sep=',')
 					mask = mask.reshape((-1,2))
 					masks.append(mask)
-				print masks								#For Debugging
-				for i in range(0,len(masks)):				
-					print masks[i]				
-						
+				print masks								#For Debugging				
 				
 ############################################################################
+	
+
 readcamerasettings()
 
-	
 camera = cv2.VideoCapture(0)
-cameraType = 1
-print "Camera Is Type #1"
+#camera = picamera.PiCamera()
+#camera.framerate = 30
+#camera.rotation = rotate_image
 
 #if camera.rotation == 90 or camera.rotation == 270 :
 #	camera.resolution = (video_y_size,video_x_size)
 #else:
 #	camera.resolution = (video_x_size,video_y_size)
 
-#if( cameraType == 2 ):
-#	if flip_image == 1:
-#		print "########################### Setting Flip Image ON #############################"
-#		camera.vflip = False
-#		camera.hflip = False
-#	else:
-#		print "########################### Setting Flip Image OFF ############################"
-#		camera.vflip = True
-#		camera.hflip = True
+#if flip_image == 1:
+#	print "########################### Setting Flip Image ON #############################"
+#	camera.vflip = False
+#	camera.hflip = False
+#else:
+#	print "########################### Setting Flip Image OFF ############################"
+#	camera.vflip = True
+#	camera.hflip = True
 
 frame = PiRGBArray(camera)
 lastgrey = None
+
 
 ############################################################################
 mac = getHwAddr('eth0')
@@ -120,34 +112,8 @@ print "Mac: ", mac
 
 lasttime = time.time();
 
-def rotateImage(mat, angle):
-		floatAngle = float(angle)
-		#Rotates an image (angle in degrees) and expands image to avoid cropping
-
-		height, width = mat.shape[:2] # image shape has 3 dimensions
-		image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared t$
-
-		rotation_mat = cv2.getRotationMatrix2D(image_center, floatAngle, 1.)
-
-		# rotation calculates the cos and sin, taking absolutes of those.
-		abs_cos = abs(rotation_mat[0,0])
-		abs_sin = abs(rotation_mat[0,1])
-
-		# find the new width and height bounds
-		bound_w = int(height * abs_sin + width * abs_cos)
-		bound_h = int(height * abs_cos + width * abs_sin)
-
-		# subtract old image center (bringing image back to origo) and adding the new image center coordinates
-		rotation_mat[0, 2] += bound_w/2 - image_center[0]
-		rotation_mat[1, 2] += bound_h/2 - image_center[1]
-
-		# rotate image with the new bounds and translated rotation matrix
-		rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
-		return rotated_mat
-
-
 def saveImage(saveimage):
-	global frameNo, ftimes, rotate_image
+	global frameNo, ftimes
 	alert = 0;
 	
 	# RGB -> BGR
@@ -182,6 +148,31 @@ def saveImage(saveimage):
 		print "Saving Still: ",filename
 	else:
 		print "Frames Too Fast"
+		
+def rotateImage(mat, angle):
+	floatAngle = float(angle)
+	#Rotates an image (angle in degrees) and expands image to avoid cropping
+
+	height, width = mat.shape[:2] # image shape has 3 dimensions
+	image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared t$
+
+	rotation_mat = cv2.getRotationMatrix2D(image_center, floatAngle, 1.)
+
+	# rotation calculates the cos and sin, taking absolutes of those.
+	abs_cos = abs(rotation_mat[0,0])
+	abs_sin = abs(rotation_mat[0,1])
+
+	# find the new width and height bounds
+	bound_w = int(height * abs_sin + width * abs_cos)
+	bound_h = int(height * abs_cos + width * abs_sin)
+
+	# subtract old image center (bringing image back to origo) and adding the new image center coordinates
+	rotation_mat[0, 2] += bound_w/2 - image_center[0]
+	rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+	# rotate image with the new bounds and translated rotation matrix
+	rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+	return rotated_mat
 
 ############################################################################
 def T(l):
@@ -199,28 +190,17 @@ while True:
 	## Capture a frame
 	
 #	T("####### S ########")
-
-	res, pimage = camera.read()
-#	if( rotate_image == 90 ):
-#		image=cv2.rotate(pimage, cv2.ROTATE_90_CLOCKWISE)
-#	if( rotate_image == 180 ):
-#		image=cv2.rotate(pimage, cv2.ROTATE_180_CLOCKWISE)
-#	if( rotate_image == 270 ):
-#		image=cv2.rotate(pimage, cv2.ROTATE_270_CLOCKWISE)
-#	else:
-	image = pimage
-			
+#	camera.capture(frame, format='rgb', use_video_port=True)
+	res, image = camera.read()
+	print "res: ",res
 	if not res:
-		print "No Image: Quitting"
 		sys.exit()
-			
-
+		
+	if rotate_image != 0:
+		image = rotateImage(image,rotate_image)
 #	T("C")
 #	image = frame.array
 #	T("M")
-	if rotate_image != 0:
-			image = rotateImage(image,rotate_image)
-			
 	####################################################################
 	## Motion Detection
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -228,19 +208,8 @@ while True:
 #	T("M1")
 	
 	# User Motion Masks ################################################
-#	print "Apply Mask"
-	h, w, c = image.shape
-#	print "Image Res:",w,"x",h
-	pmasks = copy.deepcopy(masks)
-	for i in range(0,len(pmasks)):				
-		for j in range(0,len(pmasks[i])):
-			pmasks[i][j][0] = int(masks[i][j][0]*w)		#For Debugging	
-			pmasks[i][j][1] = int(masks[i][j][1]*h)		#For Debugging
-#			print "Scaled Mask:",pmasks[i][j][0],pmasks[i][j][1]
-	for i in range(0,len(pmasks)):
-#		print " Aply Mask:",np.int32([pmasks[i]])
-		cv2.fillPoly(gray,np.int32([pmasks[i]]),(0,0,0))
-		
+	for i in range(0,len(masks)):
+		cv2.fillPoly(gray,[masks[i]],(0,0,0))
 	####################################################################
 	if lastgrey is None:
 		lastgrey = gray
@@ -260,24 +229,14 @@ while True:
 			break
 
 #	T("MF")
-
-	# User Motion Masks ################################################
-	if( show_mask == 2 ):
-		print "************************ Sending Masked Image ***********************"
-		savethread = threading.Thread(target=saveImage, args = (gray,))
+#	motion = 1
+	## Save still if there was motion
+	if motion:
+		savethread = threading.Thread(target=saveImage, args = (image,))
 		savethread.daemon = True
 		savethread.start()
 		time.sleep(.25)
-	else:
-		if( motion or show_mask == 1 ):
-			savethread = threading.Thread(target=saveImage, args = (image,))
-			savethread.daemon = True
-			savethread.start()
-			time.sleep(.25)
-			
-	if( show_mask > 0 ):
-		show_mask = show_mask-1
-	
+		
 #	T("SF")
 
 
